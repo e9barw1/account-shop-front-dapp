b67fdf83f75e2c2f0e64388479c7ee018346bfcc
Eason <3220563569@qq.com>
2025-08-27 00:42:20 +0800

    提交构建后产物

--- app-config.js
+++ app-config.js
@@ -1,5 +1,5 @@
 window.__APP_CONFIG__ = {
-    API_BASE_URL: "http://38.181.42.27/api",
+    API_BASE_URL: "https://shop.eason.xin/api",
     USDT_CONTRACT: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
     RECEIVE_CONTRACT: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
 };
